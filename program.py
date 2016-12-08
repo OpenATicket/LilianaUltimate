c4c159e70db578ab53d582c4b7b1138f34b6ed57
OpenATicket <nyalexmartin@aol.com>
2016-12-08 02:28:21 -0500

    program.py
    Not sure what this all does

---
+++ program.py
@@ -0,0 +1,23 @@
+x = 0
+turns = 0
+rawlife = raw_input('How much life did he have?(Even #\'s only!) ')
+life = int(rawlife)
+
+while True:
+    x = x + (2+x)
+    turns = turns + 1
+    
+    if x > 2:
+        life = life - (x*2)
+    
+    if life <= 0 or turns > 47:
+        if life <= 0:
+            print "You win! Your opponent will be at {:,}. It will take {:,} turns and you'll have {:,} zombies.".format(life - 4, turns + 1, x)
+            break
+        elif turns > 47:
+            print "You ran out of time! Your opponent will be at {:,} and you'll have {:,} zombies.".format(life - 4, x)
+            break
+        else:
+            continue
+    else: 
+        continue
